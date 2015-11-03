8d1a3c8e9a2719943e4095d8d7cae53ceab41131
vanjs83 <tihomir.vanjurek@gmail.com>
2015-11-03 10:15:56 +0100

    create constructor in struct S

--- var/Program.cs
+++ var/Program.cs
@@ -5,13 +5,31 @@ namespace CSharp.TipoviOperatori
     {
         public int a;
         public double b;
+        public S() {
+            a = 2;
+            b = 3.15;
+        }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            //zadatak1
+            var i0 = 5;//prevoditelj sam dodaje tip podatka ovisno kakvu smo vrijednost napisali
+            var d0 = 3.2;
+            //i0 = d0;nema implicitnog kastanja
+            //zadatak2
+            int i1 = 5;
+            double d1 = 3.2;
+            //zadatak3
+          var s=new S();
+           
 
+
+
+            
         }
     }
 }
